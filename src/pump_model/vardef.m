b57de23273f8a8a81c79b05d90a6809fd616fcee
cpokigo <cpp43@cornell.edu>
2024-04-30 15:50:24 -0400

    updates to vardef and gas properties

--- src/pump_model/vardef.m
+++ src/pump_model/vardef.m
@@ -1,1 +1,8 @@
 piston_area = 0.26;     %   [m^2]   Area of Piston
+rhoTPData = readmatrix("../../data/CO2TempData.csv");
+temp = [30;40];
+pressure = flip([30;20;10;9;8;7;6;5;4;3;2;1;0.007]);
+rhoAt30 = rhoTPData(:,2); 
+rhoAt40 = rhoTPData(:,5); 
+rho = [rhoAt30 rhoAt40]';
+
