293f092c545235e2955d201492248575fef27976
rebeccamccabe <chessgirl2400@gmail.com>
2024-07-17 12:52:57 -0400

    fixed pressure model, with mdot as input and option for fracking

--- src/energyAndTempCalcs/pressureTempCalc.m
+++ src/energyAndTempCalcs/pressureTempCalc.m
@@ -4,23 +4,24 @@ clear
 %% extra parameters
 depth = 2700;
 g = 9.81; % m/s^2
-vel_avg = 3; % m/s
+
+h_seafloor = depth;
+h_under_seafloor = h_seafloor + 200;
 
 %% calculate density of water at depth
 % density calculations
 depthShallow = 0:1001; % m
 rhoShallow = 3*10^(-6) * depthShallow + 1025; % kg/m^3
-rhoDeep = ones(1, depth-1000)*1028; % kg/m^3
+rhoDeep = ones(1, h_under_seafloor-1000)*1028; % kg/m^3
 rhoWater = [rhoShallow, rhoDeep];
 
 %% water pressure calculations
 pATM = 0.101325; % MPa
-pWater = zeros(1,depth+1);
+pWater = zeros(1,h_under_seafloor+1);
 pWater(1) = pATM;
-for i=1:depth
+for i=1:h_under_seafloor-1
     pWater(i+1) = pWater(i) + rhoWater(i) * g * 10^(-6); % by convention deltaH=1;
 end
-
 
 %% calculate allowable pressure in a pipe
 % t =;
@@ -36,57 +37,24 @@ end
 % pImpPipe = sigmaCompression*t/(r*FOS);
 
 %% calculate CO2 density at depth
+mdot = 1;
+
+[P_surface_required,P_bottom, P_vs_depth, rho_vs_depth] = get_P_surface(mdot,h_seafloor,h_under_seafloor);
 
 % assume temperature of approx 35 degree C
     % this assumption may be very wrong - we will need to discuss how CO2
     % is maintained at supercritical temperatures during pumping (may
     % because a material requirement for heat conduction/convection
     % coefficient)
-rhoCO2ref = [0 10 30 50 85 110 145 235 500 620 700 790 860 905 935]; % kg/m^3
-pCO2ref   = [0 1  2  3  4  5   6   7   8   9   10  15  20  25  30 ]; % MPa
-
-% create fit of p and rho data
-[xData, yData] = prepareCurveData( pCO2ref, rhoCO2ref);
-ft = 'linearinterp';
-opts = fitoptions( 'Method', 'LinearInterpolant' );
-try
-    opts.ExtrapolationMethod = 'linear';
-catch
-    error('You need R2023 or later to run this code')
-end
-opts.Normalize = 'on';
-[rhoVsPFit,~] = fit( xData, yData, ft, opts );
-
-% test varying values of applied pressure
-pApplied = 8; % MPa
-pC = pApplied;
-pCFxnDepth = zeros(1,depth+1);
-pCFxnDepth(1) = pC;
-fric_factor = 0.016;
-pLoss = 0.5 * vel_avg^2 * fric_factor / depth; % [Pa/(kg/m^3 * m)]
-pHydroAll = zeros(1,depth);
-pLossAll = zeros(1,depth);
-rhoAll = zeros(1,depth);
-
-%% iteration to calculate CO2 pressure at depth
-for i=1:depth
-    rhoi = rhoVsPFit(pC); % kg/m^3
-    rhoAll(i) = rhoi;
-    % note: deltahi=1 by convension
-    pHydroi = g * rhoi * 10^(-6); % MPa, iterative hydrostatic pressure
-    pLossi = pLoss * rhoi * 10^(-6); % MPa, iterative pressure loss in pipe
-    pC = pC - pLossi + pHydroi;
-    pCFxnDepth(i+1) = pC;
-    pHydroAll(i)    = pHydroi;
-    pLossAll(i)     = pLossi;
-end
 
 %% Ehina's Code for temperature
 
+
+
 %% Temperature Inputs
 % CO2_pressure_values= pCFxnDepth;
-deltaP = diff(pCFxnDepth) * 1e6; % convert MPa to Pa
-rhoCO2 = rhoAll;
+deltaP = [0 diff(P_vs_depth)]; % convert MPa to Pa
+rhoCO2 = rho_vs_depth;
 h_out    = 50; % convective heat transfer coefficient from pipe to ocean - some constant (for now) [W/(m^2 C)]
 h_in     = 50;   % convective heat transfer coefficient from CO2 to pipe [W/(m^2 C)]
 c_CO2    = 0.709; % specific heat of CO2 - some constant (for now) [J / (kg C)]
@@ -126,12 +94,12 @@ total_thermal_resistance = therm_resistance_cond_pipe + therm_resistance_cond_in
 
 %% Temperature Calculation
 % create empty lists to populate
-T_CO2   = zeros(1,depth+1); % temperature of CO2
+T_CO2   = zeros(1,h_under_seafloor+1); % temperature of CO2
 
 %initialize TCO2 at the surface of the ocean
 T_CO2(1) = 31; % minimum value to maintain supercritical state [C]
 
-for i = 1:depth
+for i = 1:h_under_seafloor
     heat_xfer_from_CO2_to_ocean = 1/total_thermal_resistance * (T_CO2(i) - T_oc);
     net_heat_xfer_into_CO2  = -heat_xfer_from_CO2_to_ocean + g * deltaZ + deltaP(i) / rhoCO2(i) + P_heat;
 
@@ -151,9 +119,9 @@ T_insu  = T_oc + (T_CO2 - T_oc) * R_ratio_T_insu;
 %% Plot temperature and pressure of CO2 as a function of depth
 figure
 %Pressure
-x = 0:depth;
-y1 = pCFxnDepth; 
-y2 = T_CO2;
+x = 1:h_under_seafloor;
+y1 = P_vs_depth*1e6; 
+y2 = T_CO2(2:end);
 
 yyaxis left
 plot(x,y1,'LineWidth', 1.5, 'Color','#0072BD','LineStyle',"-");
@@ -174,11 +142,11 @@ plot([0,depth],[pInjection,pInjection] ,'LineWidth',1.5, 'Color', '#77AC30', 'Li
 % constraint: maintain supercritical fluid
 pSupercritical = 7.37; % MPa requirement
 plot( [0, depth], [pSupercritical, pSupercritical],'LineWidth',1.5, 'Color', '#77AC30','LineStyle',"-")
-plot(x,pWater,'LineWidth',1.5, 'Color','#EDB120', 'LineStyle',"-")
+plot(x,pWater(2:end),'LineWidth',1.5, 'Color','#EDB120', 'LineStyle',"-")
 
 % contsraint: no pipe explosion
 % constraint: no pipe implosion
-pDifferential = pWater - pCFxnDepth;
+pDifferential = pWater(2:end) - P_vs_depth*1e6;
 pDiffMax = max(pDifferential);
 pDiffMin = min(pDifferential);
 
@@ -188,7 +156,64 @@ legend('CO2 Pressure Chart','Injection MINIMUM Requirment', ...
 %% Plot all temperatures as a function of depth
 
 figure
-plot(x, T_CO2, x, T_s_in, '--', x, T_s_out, '-.', x, T_insu,'LineWidth',1.5)
+plot(x, T_CO2(2:end), x, T_s_in(2:end), '--', x, T_s_out(2:end), '-.', x, T_insu(2:end),'LineWidth',1.5)
 legend('CO2','Steel pipe inside','Steel pipe outside','Insulation outside')
 xlabel('Depth (m)')
 ylabel('Temperature (C)')
+
+function [P_surface_required,P_bottom, P_vs_depth, rho_vs_depth] = get_P_surface(mdot,h_seafloor,h_under_seafloor)
+    
+    P_surface_guess = 8e6; % 8 MPa
+    g = 9.8;
+    rho_w = 1000;
+
+    % iterate until P_surface solves the nonlinear equation
+    P_bottom_required = rho_w * g * h_under_seafloor;
+    P_bottom_error_fcn = @(P_surface_guess) (pressure_vs_depth_fcn(mdot,P_surface_guess,h_seafloor,h_under_seafloor) - P_bottom_required);
+    P_surface_required = fzero(P_bottom_error_fcn, P_surface_guess);
+
+    % rerun result with converged P_surface
+    [P_bottom, P_vs_depth, rho_vs_depth] = pressure_vs_depth_fcn(mdot,P_surface_required,h_seafloor,h_under_seafloor);
+
+end
+
+function [P_bottom, P_vs_depth, rho_vs_depth] = pressure_vs_depth_fcn(mdot,P_surface,h_seafloor,h_under_seafloor)
+    rhoCO2ref = [0 10 30 50 85 110 145 235 500 620 700 790 860 905 935]; % kg/m^3
+    pCO2ref   = [0 1  2  3  4  5   6   7   8   9   10  15  20  25  30 ]*1e6; % Pa
+
+    g = 9.8;
+    D = 1;
+
+    P_vs_depth = zeros(1,h_under_seafloor);
+    P_vs_depth(1) = P_surface;
+    rho_vs_depth = zeros(1,h_under_seafloor);
+    
+    %% iteration to calculate CO2 pressure at depth
+    for i=2:h_under_seafloor
+        rho_i = interp1(pCO2ref, rhoCO2ref, P_vs_depth(i-1),'linear','extrap'); % density of CO2, kg/m^3
+        mu_i = 1; % dynamic viscosity of CO2
+
+        % note: delta_h_i=1 by convention
+        deltaP_hydro_i = g * rho_i; % Pa, iterative hydrostatic pressure
+        if i < h_seafloor % pipe loss
+            deltaP_loss_i = 128 * mdot / (pi * D^4) * mu_i / rho_i^2; % assumes laminar flow so f = 64/Re
+        else % injection loss
+            fracking = false;
+            if fracking
+                delta_P_loss_i = P_fracking;
+            else % linear Darcy loss based on rock permeability
+                C = 1;
+                delta_P_loss_i = C * mdot;
+            end
+        end
+        deltaP_i = deltaP_hydro_i - deltaP_loss_i;
+
+        rho_vs_depth(i) = rho_i;
+        P_vs_depth(i) = P_vs_depth(i-1) + deltaP_i;
+    end
+    P_bottom = P_vs_depth(end);
+
+    rho_vs_depth(1) = rho_vs_depth(2);
+
+end
+
